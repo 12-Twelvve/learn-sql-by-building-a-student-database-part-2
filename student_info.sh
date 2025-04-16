e57cfc61c77ce07a9abb3c1c65e1e43763caae9b
Kuber Budhathoki <koobear99@gmail.com>
2025-04-16 17:32:00 +0000

    Save progress: 2420.1

--- student_info.sh
+++ student_info.sh
@@ -34,3 +34,5 @@ echo -e "\nList of majors, in alphabetical order, that either no student is taki
 echo "$($PSQL "select major from majors left join students on majors.major_id=students.major_id where student_id is null or first_name ilike '%ma%' order by major")"
 
 echo -e "\nList of unique courses, in reverse alphabetical order, that no student or 'Obie Hilpert' is taking:"
+echo "$($PSQL "SELECT DISTINCT(course) FROM students RIGHT JOIN majors USING(major_id) INNER JOIN majors_courses USING(major_id) INNER JOIN courses USING(course_id) WHERE (first_name = 'Obie' AND last_name = 'Hilpert') OR student_id IS NULL ORDER BY course DESC")"
+# echo -e "$($PSQL "select distinct(course) from students right join majors using(major_id) inner join  majors_courses using(major_id) inner join courses using(course_id) where (first_name='Obie' and last_name='Hilpert') student_id is null  order by course desc")"
